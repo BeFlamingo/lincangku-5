18180469dde5971aca9e3db92436c0363a7ad93a
BeFlamingo <@DESKTOP-HTGGN89>
2020-11-03 16:17:05 +0800

    Homeworkfive

---
+++ src/ninepointten/QuadraticEquation.java
@@ -0,0 +1,50 @@
+package ninepointten;
+
+public class QuadraticEquation {
+	private double a;
+    private double b;
+    private double c;
+ 
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+ 
+
+    public double getA() {
+        return a;
+    }
+    public double getB() {
+        return b;
+    }
+    public double getC() {
+        return c;
+    }
+ 
+
+    public double getDiscriminant() {
+        double discriminant = b * b - 4 * a * c;
+        return discriminant;
+    }
+ 
+    public double getRoot1() {
+        if (getDiscriminant() < 0)
+            return 0;
+        else {
+            double root1 = (-b + Math.sqrt(getDiscriminant())) / 2 * a;
+            return root1;
+        }
+    }
+        public double getRoot2 () {
+            if (getDiscriminant() < 0)
+                return 0;
+            else {
+                double root2 = (-b - Math.sqrt(getDiscriminant())) / 2 * a;
+                return root2;
+            }
+        }
+
+	
+}
